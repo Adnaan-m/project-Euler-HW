f0523ea40c37fc59acbfaeb11ae5c518f249ad77
Tech-A08 <tech-a08@MacBook-Air-3.local>
2018-09-25 17:48:39 +0100

    completed two questions

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 ###Description
 The following ReadMe displays the information regarding the code used to solve several tasks presented from the Project Euler website. [Click Here](https://projecteuler.net) to see the website. 
+
+### Languages used
+* Ruby
+
+### How to download
+1. Search for Adnaan-m in GitHub. [Click Here](https://github.com/Adnaan-m/project-Euler-HW) for the link.
+2. Search for the project-Euler-HW repository.
+3. Download the files displayed.
--- app.rb
+++ app.rb
@@ -1,13 +1,10 @@
 # The sum of the squares of the first ten natural numbers is,
-#
 # 1^2 + 2^2 + ... + 10^2 = 385
 # The square of the sum of the first ten natural numbers is,
-#
 # (1 + 2 + ... + 10)2 = 552 = 3025
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-#
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-
+#
 sum1=[]
 sum2=[]
 for i in 1..100
@@ -22,3 +19,13 @@ end
 p total2 = (sum2.sum) **2
 
 p "The total is: #{total = total2 - total1}"
+
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+#
+for j in 2500000..300000000
+  if j % 1 == 0 && j % 2 == 0 && j % 3 == 0 && j % 4 == 0 && j % 5 == 0 && j % 6 == 0 && j % 7 == 0 && j % 8 == 0 && j % 9 == 0 && j % 10 == 0 && j % 11 == 0 && j % 12 == 0 && j % 13 == 0 && j % 14 == 0 && j % 15 == 0 && j % 16 == 0 && j % 17 == 0 && j % 18 == 0 && j % 19 == 0 && j % 20 == 0
+    p j
+  end
+end
